91c5ef79f35f2219494397f80b7906b17add07f6
Jordon R <reese.jordon@gmail.com>
2018-02-07 18:07:33 -0600

    Small commit notifications comments

--- client/src/components/CreateNotification.js
+++ client/src/components/CreateNotification.js
@@ -1,9 +1,29 @@
-<<<<<<< HEAD
-=======
 // Used to create a custome post on the activity feed
 
 
-import React from "react";
+import React, {Component} from "react";
+
+class NotificationEntry extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      actor: "Jordon",    // The creator of the notification
+      verb: "liked",      // The action being made by the creator
+      object: "comment",  // The object for which they are performing the action on
+      target: "Craig",    // Whose notification array is updated
+      visible: true,      // Have notifications been hovered over?
+    }
+  }
+
+
+  render() {
+    return (
+      
+
+    )
+  }
+}
+
 
 
 
--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -3,6 +3,7 @@ import React from "react";
 // import {Link} from "react-router-dom";
 // import PropTypes from "prop-types";
 import { NavDropdown, MenuItem } from "react-bootstrap";
+import CreateNotification from "./CreateNotification";
 
 
 function Notifications(props) {
